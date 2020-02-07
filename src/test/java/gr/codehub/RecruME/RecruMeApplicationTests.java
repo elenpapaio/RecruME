b2a530fe84428dac5abf07b708301c5f470991bf
Eleni Papaioannou <eleni.papaioannou90@gmail.com>
2020-02-07 13:42:09 +0200

    Initial commit

---
+++ src/test/java/gr/codehub/RecruME/RecruMeApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.codehub.RecruME;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
